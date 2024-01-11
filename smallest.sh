bf4f3d16e3a3738923c86a97f8f7545c35f73880
Deepikavin <116055801+Deepikavin@users.noreply.github.com>
2024-01-11 17:00:27 +0000

    Created smallest.sh

---
+++ smallest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo $1 $2
+echo $num1
+echo "enter the value of a:"
+read a
+echo "enter the value of b:"
+read b
+if [ $a -lt $b]
+then 
+      echo "the value a is smaller than b"
+else
+      echo "the value b is smaller than a"
+fi
